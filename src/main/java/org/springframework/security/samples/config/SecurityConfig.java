b0bc290d2d900c1bd7b2955db24d42e2ca178e8d
AmauryEsparza <amauryesparza@gmail.com>
2015-06-05 17:51:09 -0500

    Just commiting

---
+++ src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.springframework.security.samples.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	// @formatter:off
+	@Autowired
+	public void configureGlobal(
+			AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}
+	// @formatter:on
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated()
+				.and()
+			.formLogin().loginPage("/login").permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+}
